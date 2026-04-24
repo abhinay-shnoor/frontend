7b6560e769fc044f5e9e54dcc8ec2ad17eeda333
Abhinay <abhinay@shnoor.com>
2026-04-24 17:37:52 +0530

    Cleanup: remove redundant AuthProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
+
   </StrictMode>
 );
